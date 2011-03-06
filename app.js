eeb1caadecdfa32c5f064685e7a70af57c6955f8
Richard Anaya <richard.anaya@gmail.com>
2011-03-05 20:07:20 -0600

    using correct port

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var app = [
 ];
 
 var server = nerve.create(app, {session_duration: 10000, document_root: './static'})
-server.listen(80);
+server.listen(8757);
 
 //WebSocket Setup, delete below this line if you don't use
 /*var io = require('socket.io');
